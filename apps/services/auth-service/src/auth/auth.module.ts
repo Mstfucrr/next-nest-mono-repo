818524ddd50a47b49a7bb350073568b86217a843
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-07-19 01:17:08 +0300

    Update package.json scripts for services and enhance auth module documentation in NestJS; rename dev and build scripts for clarity, and add comments for better understanding of JWT configuration.

--- apps/services/auth-service/src/auth/auth.module.ts
+++ apps/services/auth-service/src/auth/auth.module.ts
@@ -6,16 +6,18 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }), // Bu modülün global olarak kullanılmasını sağlar
     JwtModule.registerAsync({
+      // JWT konfigürasyonu
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') }
+        // JWT_SECRET ve JWT_EXPIRES_IN değerlerini alır ve JwtModule'a gönderir
+        secret: config.get<string>('JWT_SECRET'), // JWT_SECRET değerini alır
+        signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') } // JWT_EXPIRES_IN değerini alır
       }),
-      inject: [ConfigService]
+      inject: [ConfigService] // Burada ConfigService'i inject ediyoruz. inject: [ConfigService] ile ConfigService'i kullanabiliriz.
     })
   ],
-  controllers: [AuthController],
-  providers: [AuthService]
+  controllers: [AuthController], // Auth controller
+  providers: [AuthService] // Auth service
 })
 export class AuthModule {}
