b18c1672561edb57dcf032b5db9ad5b99c7fc265
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-07-24 11:11:02 +0300

    Add API Gateway service with NestJS structure, including controllers, services, and modules for authentication and user management. Integrate ESLint and Prettier for code quality, and set up testing configurations for unit and e2e tests.

---
+++ apps/services/api-gateway/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthController } from './auth/auth.controller'
+import { AuthModule } from './auth/auth.module'
+import { UserController } from './user/user.controller'
+import { UserModule } from './user/user.module'
+
+@Module({
+  imports: [AuthModule, UserModule],
+  controllers: [AppController, AuthController, UserController],
+  providers: [AppService]
+})
+export class AppModule {}
