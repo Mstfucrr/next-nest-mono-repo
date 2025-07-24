8746a23049429ae1380b6f2b19ecf6e4880450c2
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-07-24 11:19:17 +0300

    Add global validation and Swagger documentation setup in API Gateway. Introduce VSCode settings for code formatting and linting with Prettier.

--- apps/services/api-gateway/src/main.ts
+++ apps/services/api-gateway/src/main.ts
@@ -1,8 +1,28 @@
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const config = app.get(ConfigService)
+
+  // Global validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  const opts = new DocumentBuilder()
+    .setTitle('Dailyshop API Gateway')
+    .setVersion('1.0')
+    .addTag('auth')
+
+    .addTag('user')
+    .addBearerAuth()
+    .build()
+
+  const doc = SwaggerModule.createDocument(app, opts)
+  SwaggerModule.setup('docs', app, doc)
+
+  await app.listen(config.get<number>('PORT') ?? 3000)
 }
 bootstrap()
