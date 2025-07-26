15ec207c7c6aa1ce56037e23ecc0e0dbb21b6988
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-07-27 00:30:02 +0300

    Implement microservice architecture for product service
    - Refactored the product service to utilize NestJS microservices with TCP transport.
    - Configured service options including host and port from environment variables.
    - Integrated AppLogger for improved logging during service startup.
    - Updated bootstrap function to handle microservice initialization and logging.

--- apps/services/product-service/src/main.ts
+++ apps/services/product-service/src/main.ts
@@ -1,8 +1,25 @@
+import { AppLogger } from '@dailyshop/shared-utils'
 import { NestFactory } from '@nestjs/core'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+const microserviceOptions: MicroserviceOptions = {
+  transport: Transport.TCP,
+  options: {
+    host: process.env.PRODUCT_HOST || '127.0.0.1',
+    port: parseInt(process.env.PRODUCT_PORT || '4003')
+  }
 }
-bootstrap()
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    ...microserviceOptions,
+    bufferLogs: true
+  })
+  const logger = await app.resolve(AppLogger)
+  app.useLogger(logger)
+  logger.log('Product service is running on port 4003')
+  await app.listen()
+}
+
+void bootstrap()
