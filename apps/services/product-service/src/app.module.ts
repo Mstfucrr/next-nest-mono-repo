14c3ab5f70cce3e792a74b15bb9344db373a9744
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-07-27 03:19:12 +0300

    Update Jest configuration and integrate Prisma module in app module
    - Refactored jest.config.js for improved readability by removing unnecessary line breaks.
    - Updated app.module.ts to include PrismaModule and PrismaService for enhanced database management.

--- apps/services/product-service/src/app.module.ts
+++ apps/services/product-service/src/app.module.ts
@@ -2,11 +2,13 @@ import { AppLogger } from '@dailyshop/shared-utils'
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
+import { PrismaService } from './prisma/prisma.service'
 import { ProductModule } from './product/product.module'
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, AppLogger]
+  providers: [AppService, PrismaService, AppLogger]
 })
 export class AppModule {}
