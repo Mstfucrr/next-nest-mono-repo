06555eb08a4a47c6a343d63ca69707563afe20bb
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-07-30 01:18:17 +0300

    chore: enhance entrypoint script and update tsconfig settings
    - Updated the entrypoint script in product-service to include detailed logging for the shared-utils package and its contents.
    - Modified tsconfig.json files for shared-utils and shared-types to specify the root directory as 'src' for better project structure.

--- apps/services/product-service/entrypoint.sh
+++ apps/services/product-service/entrypoint.sh
@@ -6,11 +6,22 @@ echo "==== /app içeriği ===="
 tree /app -L 1
 echo "==== /app/dist içeriği ===="
 tree /app/dist
-# echo "==== /app/node_modules içeriği ===="
-# tree /app/node_modules -L 1
-echo "==== /app/dist/libs/shared-utils/src içeriği ===="
-tree /app/dist/libs/shared-utils/src
+echo "==== /app/node_modules/@dailyshop/shared-utils/dist içeriği ===="
+tree /app/node_modules/@dailyshop/shared-utils/dist -L 1
+
+
+
+
+#app/node_modules/@dailyshop/shared-utils/package.json
+echo "==== /app/node_modules/@dailyshop/shared-utils/package.json içeriği ===="
+cat /app/node_modules/@dailyshop/shared-utils/package.json
+
+
+
+echo "==== /app/node_modules/@dailyshop/shared-utils içeriği ===="
+tree /app/node_modules/@dailyshop/shared-utils
 # echo "==== /app/dist/apps/services/product-service içeriği ===="
 # tree /app/dist/apps/services/product-service
+echo "========================================="
 echo "==== Çalıştırılıyor: $@ ===="
 exec "$@"
